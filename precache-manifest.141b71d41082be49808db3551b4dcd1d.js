91035ec997422f8a1c87b8b01f631022c9b726b5
Daud Semab <daudsemab.dxb@gmail.com>
2020-03-24 16:32:50 +0500

    Updates

---
+++ precache-manifest.141b71d41082be49808db3551b4dcd1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a6132ff419cdcb0fa44541132105bf",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0127e38062b22234924f",
+    "url": "/robofriends/static/css/main.1928aa85.chunk.css"
+  },
+  {
+    "revision": "14779e1b45c99435fc9e",
+    "url": "/robofriends/static/js/2.771c916d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends/static/js/2.771c916d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0127e38062b22234924f",
+    "url": "/robofriends/static/js/main.324aa76b.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
